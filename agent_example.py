e0cf48455722c58e849b6ad323c523465b444e23
NiklasK05 <derbote@gmx.de>
2024-11-15 08:57:32 +0100

    first test prompt engineering with agent

--- agent_example.py
+++ agent_example.py
@@ -12,35 +12,34 @@ llm_config = {
     "api_key": os.environ.get("OPENAI_API_KEY"),
     "azure_endpoint": os.environ.get("OPENAI_AZURE_ENDPOINT"),
     "api_type": "azure", 
-    "api_version": os.environ.get("OPENAI_API_VERSION")
-
+    "api_version": os.environ.get("OPENAI_API_VERSION"),
+    "temperature": 0.1
 }
 
+image_path = "GenAI-Seminar-UC1\Grundriss Beispiele\Grundriss_02_OG.jpg"
 
 task = '''
-        Write a concise blogpost about the positive aspects of the job as an architect. Make sure the blogpost is within 100 words.
+        Your task is: take the given floor plan in the variable image_path and transform it into a cad-file with ezdxf.
         '''
 
-writer = ConversableAgent(
-    name = "writer",
-    system_message="You are a writer. You write engaging and concise " 
-        "blogpost (with title) on given topics. You must polish your "
-        "writing based on the feedback you receive and give a refined "
-        "version. Only return your final work without additional comments.",
+prompt_engineer = ConversableAgent(
+    name = "prompt_engineer",
+    system_message="You are a Prompt Engineer. You are able to develop a prompting strategy that is suitable for the given task. Give a clear and structured prompt that allows gpt-4o-mini to solve my task"
+        "Explain the choice of prompt and how it works",
     llm_config= llm_config
 )
-reply = writer.generate_reply(messages=[{"content": task, "role": "user"}])
+reply = prompt_engineer.generate_reply(messages=[{"content": task, "role": "user"}])
 
 print(reply)
 
 critic = ConversableAgent(
     name = "critic",
     is_termination_msg=lambda x: x.get("content", "").find("TERMINATE") >= 0,
-    system_message="You are a critic. You review the work of the writer and provide constructive feedback to help improve the quality of the content.",
+    system_message="You are a critic. You review the work of the prompt engineer and provide constructive feedback to help improve the quality of the provided prompt.",
     llm_config= llm_config
 )
 res = critic.initiate_chat(
-    recipient=writer,
+    recipient=prompt_engineer,
     message=task,
     max_turns=2,
     summary_method="last_msg"
