e0a27221bb078136ca416f10963358c9ce231310
Oskar Wresinski <wresinski@gmx.net>
2025-01-02 13:09:18 +0100

    update d-w_finder.py

--- code/d-w_finder.py
+++ code/d-w_finder.py
@@ -3,7 +3,7 @@ import json
 from PIL import Image
 import os
 from dotenv import load_dotenv
-load_dotenv
+load_dotenv()
 
 def convert_to_jpg(input_path, output_path):
     # Öffne das Bild
@@ -22,11 +22,6 @@ def call_roboflow(image, confidence, overlap):
     project = rf.workspace().project("segmenting-a-floor-plan")
     model = project.version("1").model
 
-    # optionally, change the confidence and overlap thresholds
-    # values are percentages
-    model.confidence = confidence
-    model.overlap = overlap
-
     # predict on a local image
     # prediction = 
     return model.predict(image)
@@ -42,10 +37,10 @@ def save_json(data, path_to):
 # Predict on a hosted image via URL
 #prediction = model.predict("https://...", hosted=True)
 
-def main():
-    convert_to_jpg(r"Neue Grundrisse\D-Str\D-Str. Obergeschoss.png", "segment_floorplan.jpg")
+def main(image=r"Neue Grundrisse\image.png"):
+    convert_to_jpg(image, r"data\segment_floorplan.jpg")
 
-    prediction = call_roboflow("segment_floorplan.jpg", 0, 25)
+    prediction = call_roboflow(r"data\segment_floorplan.jpg", 50, 50)
 
     # Plot the prediction in an interactive environment
     prediction.plot()
@@ -53,7 +48,7 @@ def main():
     # Convert predictions to JSON
     data = prediction.json()
     
-    save_json(data, "segment_floorplan.json")
+    save_json(data, r"data\segment_floorplan.json")
 
 if __name__ == "__main__":
     main()
